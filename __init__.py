505a4f27398bedb82eef45c1c01c15714d207315
Lucas Emanoel <lucas2014.barros@gmail.com>
2025-04-03 20:43:08 -0300

    CHG: new folder structure

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .Lexer import Lexer
-from .Parser import Parser
-from .SymbolTable import SymbolTable
--- src/compilador.py
+++ src/compilador.py
@@ -2,11 +2,11 @@ from Lexer import Lexer
 from Parser import Parser
 
 if __name__ == "__main__":
-    lexer = Lexer("code3.meme")
+    lexer = Lexer("input/code3.meme")
     lexer.start()
-    #lexer.display_tokens()
+    lexer.display_tokens()
     lexer.display_symbol_table()
 
     print('Leitura no Parser')
-    #parser = Parser(lexer.tokens, lexer.symbol_table)
-    #parser.start()
+    parser = Parser(lexer.tokens, lexer.symbol_table)
+    parser.start()
