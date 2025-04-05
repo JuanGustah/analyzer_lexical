d9001f4334e5d463ff54590084bfb2f6b9457eac
Lucas Emanoel <lucas2014.barros@gmail.com>
2025-04-05 17:17:50 -0300

    feat: dumb reader of Contexts using lexer

---
+++ src/Context.py
@@ -0,0 +1,43 @@
+from SymbolTable import SymbolTable
+
+class Context:
+    def __init__(self, identifier: str, parent=None):
+        self.identifier = identifier
+        self.symbol_table = SymbolTable()
+        self.context_counter = 1
+        self.subcontexts = [] 
+        self.parent = parent 
+
+    def add_subcontext(self, subcontext_name: str):
+        new_subcontext = Context(subcontext_name, parent=self)
+        self.context_counter += 1
+        self.subcontexts.append(new_subcontext)
+        return new_subcontext
+
+    def get_subcontext(self, identifier: str):
+        for sub in self.subcontexts:
+            if sub.identifier == identifier:
+                return sub
+        return None
+
+    def add_symbol(self, id_name: str, linha: int, coluna: int):
+        return self.symbol_table.add(id_name, linha, coluna)
+    
+    def lookup(self, id_name: str):
+        idx = self.symbol_table.lookup(id_name)
+        if idx is not None:
+            return self, idx  # Retorna o contexto e o índice do símbolo
+        if self.parent:
+            return self.parent.lookup(id_name)  # Busca no contexto pai
+        return None, None  # Não encontrado na hierarquia
+
+    def list_symbols(self):
+        print(f"Contexto: {self.identifier}")
+        self.symbol_table.list()
+        
+        for subcontext in self.subcontexts:
+            subcontext.list_symbols()
+            
+    def generate_unique_name(self, base_name):
+      unique_name = f"{self.context_counter}_{base_name}"
+      return unique_name
--- src/compilador.py
+++ src/compilador.py
@@ -2,14 +2,15 @@ from Lexer import Lexer
 from Parser import Parser
 
 if __name__ == "__main__":
-    lexer = Lexer("input/input.meme")
+    lexer = Lexer("input/code2.meme")
     lexer.start()
     #lexer.display_tokens()
-
+    lexer.context.list_symbols()
+    
     print('Leitura no Parser')
-    parser = Parser(lexer.tokens, lexer.symbol_table)
-    parser.start()
+    #parser = Parser(lexer.tokens, lexer.symbol_table)
+    #parser.start()
 
-    lexer.display_symbol_table()
+    #lexer.display_symbol_table()
 
     # parser.print_all()
